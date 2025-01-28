ac52ab295b7d330fb64f64f378937ba1b45bbae9
BigJimThunder <128534319+BigJimThunder@users.noreply.github.com>
2025-01-28 02:19:44 +0000

    whole game

--- app.rb
+++ app.rb
@@ -2,8 +2,50 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:homepage)
+end
+
+get("/rock") do
+  @outcome = "We Won!"
+  rps = ["rock", "paper", "scissors"]
+  @choice = "rock"
+  @cpu_choice = rps.sample
+
+  if @choice == @cpu_choice
+    @outcome = "We tied!"
+  elsif @cpu_choice == "paper"
+    @outcome = "We lost!"
+  end
+
+  erb(:game)
+end
+
+get("/paper") do
+  @outcome = "We Won!"
+  rps = ["rock", "paper", "scissors"]
+  @choice = "paper"
+  @cpu_choice = rps.sample
+
+  if @choice == @cpu_choice
+    @outcome = "We tied!"
+  elsif @cpu_choice == "scissors"
+    @outcome = "We lost!"
+  end
+
+  erb(:game)
+end
+
+get("/scissors") do
+  @outcome = "We Won!"
+  rps = ["rock", "paper", "scissors"]
+  @choice = "scissors"
+  @cpu_choice = rps.sample
+
+  if @choice == @cpu_choice
+    @outcome = "We tied!"
+  elsif @cpu_choice == "rock"
+    @outcome = "We lost!"
+  end
+
+  erb(:game)
 end
